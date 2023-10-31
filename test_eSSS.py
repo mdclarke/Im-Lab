80f925f62c400480f08c84a0c7bbd76f0ab9b7e4
Maggie Clarke <mdclarke@uw.edu>
2023-10-31 12:57:11 -0700

    Update test_eSSS.py

--- test_eSSS.py
+++ test_eSSS.py
@@ -3,8 +3,9 @@
 """
 Created on Tue Aug  8 12:43:54 2023
 
-@author: maggie
+@author: maggie clarke
 
+Test eSSS for supression of vibrational artifacts
 """
 import mne
 import os.path as op
@@ -19,20 +20,22 @@ fc = op.join(path, 'sss_cal.dat')
 # read in raw data
 raw = mne.io.Raw(op.join(path, 'sam', 'pilot_4_task_1_raw.fif'), preload=True)
 
+# perform regular tSSS for comparison
 tsss = maxwell_filter(raw, calibration=fc, cross_talk=ct, st_duration=10)
 
-# here we will bandapss the empty room around the 29.5Hz projector artifact
+# here we will bandapss the empty-room data around the 29.5Hz vibration artifact
 erm = mne.io.Raw(op.join(path, 'sam', 'empty_room.fif'),
                  preload=True).filter(l_freq=27, h_freq=31)
 
-## compute erm projectors to use for eSSS
+## compute erm projectors from empty-room to use for eSSS
 erm_proj = mne.compute_proj_raw(erm, meg='combined')
 # plot the combined projectors
 mne.viz.plot_projs_topomap(erm_proj, colorbar=True,
                            info=erm.info)
-# perform eSSS
+# perform eSSS on raw data
 esss = maxwell_filter(raw, calibration=fc, cross_talk=ct, st_duration=10, 
                       extended_proj=erm_proj)
-# plot
+
+# plot tSSS and eSSS spectra to ensure the artifact is suppressed
 tsss.plot_psd(fmin=1,fmax=55)
 esss.plot_psd(fmin=1,fmax=55)
